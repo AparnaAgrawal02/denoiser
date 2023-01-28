60a5dd8f44be07b3117ba752e01ad30f15e2ba2e
Aparna <sanagrawal10401@gmail.com>
2023-01-28 13:12:49 +0530

    added file to convert to wav

---
+++ .github/convert_to_wav.py
@@ -0,0 +1,31 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(
+        "denoiser.demucs",
+        description="Benchmark the streaming Demucs implementation, "
+                    "as well as checking the delta with the offline implementation.")
+parser.add_argument("--path", default="", type=str)
+args = parser.parse_args()
+
+from pydub import AudioSegment
+
+formats_to_convert = ['.m4a']
+
+for (dirpath, dirnames, filenames) in os.walk(args.path):
+    for filename in filenames:
+        if filename.endswith(tuple(formats_to_convert)):
+
+            filepath = dirpath + '/' + filename
+            (path, file_extension) = os.path.splitext(filepath)
+            file_extension_final = file_extension.replace('.', '')
+            try:
+                track = AudioSegment.from_file(filepath,
+                        file_extension_final)
+                wav_filename = filename.replace(file_extension_final, 'wav')
+                wav_path = dirpath + '/' + wav_filename
+                print('CONVERTING: ' + str(filepath))
+                file_handle = track.export(wav_path, format='wav')
+                os.remove(filepath)
+            except:
+                print("ERROR CONVERTING " + str(filepath))
